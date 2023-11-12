e1415d637623d6340548fd7a3b11ecab46532282
MisterLobo <algae1234567890@gmail.com>
2023-11-12 16:13:38 +0800

    move functions to common

--- src/common.py
+++ src/common.py
@@ -1,3 +1,26 @@
+from platformdirs import *
+from dotenv import load_dotenv
+from os import path
+import os
+
+load_dotenv()
+
+DOCKER_HOST=os.getenv('DOCKER_HOST')
+APP_VERSION=os.getenv('APP_VERSION')
+APP_NAME=os.getenv('APP_NAME')
+CONFIG_PATH_DEFAULT=os.getenv('CONFIG_PATH_DEFAULT')
+
+def gettmpdir():
+    return f'{user_config_dir(APP_NAME)}/tmp'
+
+def getconfigpath():
+    return f'{user_config_dir(APP_NAME)}/{CONFIG_PATH_DEFAULT}'
+
+def checkpaths():
+    dirname = gettmpdir()
+    if not path.isdir(dirname):
+        os.mkdir(dirname)
+
 class ModelType:
     CONTAINER = 'CONTAINER'
     IMAGE = 'IMAGE'
